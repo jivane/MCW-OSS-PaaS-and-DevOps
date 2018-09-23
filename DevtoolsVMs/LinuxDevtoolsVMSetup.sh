020a276218f4bb484584ba72deed8ca816095c43
Francois X <kfx@free.fr>
2018-09-23 20:55:37 +0200

    Update LinuxDevtoolsVMSetup.sh
    Add additionnal tools

--- DevtoolsVMs/LinuxDevtoolsVMSetup.sh
+++ DevtoolsVMs/LinuxDevtoolsVMSetup.sh
@@ -37,12 +37,13 @@ sudo service mongod start
 
 #install docker
 #to run the az cli container open terminal and use 'sudo docker run -it azuresdk/azure-cli-python:latest'
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
+apt-get install -y apt-transport-https ca-certificates curl
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 apt-get update
 apt-get install -y docker-ce
+
+# Install additionnal tools
+apt-get install -y fish terminator
+
