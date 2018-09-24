b67a7c9b14b2085e297b511b9d841498d0b9fd99
Francois X <kfx@free.fr>
2018-09-24 15:13:58 +0200

    Create WinDevToolsSetup.cmd

---
+++ DevtoolsVMs/WinDevToolsSetup.cmd
@@ -0,0 +1,27 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install -y -f 7zip.install
+choco install -y -f azure-cli
+choco install -y -f azurepowershell
+choco install -y -f microsoftazurestorageexplorer
+choco install -y -f chocolateygui
+choco install -y -f composer
+choco install -y -f curl
+choco install -y -f ditto.install
+choco install -y -f docker-for-windows
+choco install -y -f docker-kitematic
+choco install -y -f dotnet4.7
+choco install -y -f git.install
+choco install -y -f greenshot
+choco install -y -f markdownmonster
+choco install -y -f MobaXTerm
+choco install -y -f mysql
+choco install -y -f nircmd
+choco install -y -f nodejs.install
+choco install -y -f notepad2
+choco install -y -f postgresql
+choco install -y -f powershell
+choco install -y -f sumatrapdf.install
+choco install -y -f sysinternals
+choco install -y -f visualstudiocode
+choco install -y -f wget
+choco install -y -f winscp
